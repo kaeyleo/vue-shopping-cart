cdad33d06cc3236e798d4ef927d248cf621c6a06
steve liao <mynamelky@gmail.com>
2017-09-01 15:06:15 +0800

    refactor code & create two instances

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-new Vue({
-    el: '#app',
+var List = new Vue({
+    el: '#page-list',
     data: {
-        cate_index: 0,
+        cate_index: 0, // 当前分类项下标
+        filter_index: 0, // 当前条件筛选项下标
+        price_isAsc: false, // 价格是否升序
         cate: [
             { id: 0, des: '推荐' },
             { id: 1, des: '母婴' },
@@ -10,7 +12,6 @@ new Vue({
             { id: 4, des: '数码家电' },
             { id: 5, des: '居家百货' }
         ],
-        filter_index: 0,
         goods: [{
             id: 1001,
             name: 'Beats EP头戴式耳机',
@@ -100,25 +101,29 @@ new Vue({
             sales: 18,
             img: 'http://img.alicdn.com/imgextra/i3/TB16avCQXXXXXcsXpXXXXXXXXXX_!!0-item_pic.jpg_400x400q60s30.jpg_.webp'
         }],
-        list: [],
-        price_isAsc: false,
-        cart: [{
-            id: 1001,
-            name: 'Beats EP头戴式耳机',
-            price: 558,
-            type: 4,
-            quantity: 1,
-            subtotal: '558.0',
-            stock: 128,
-            sales: 1872,
-            img: 'http://img11.360buyimg.com/n1/s528x528_jfs/t3109/194/2435573156/46587/e0e867ac/57e10978N87220944.jpg!q70.jpg'
-        }]
+        list: []
     },
     created: function() {
         this.setList()
     },
     methods: {
-        // 切换商品分类
+
+        /**
+         * @method 设置商品列表
+         */
+        setList: function() {
+            var self = this;
+            this.list = this.goods.filter(function(el) {
+                return self.cate_index !== 0 ?
+                    el.type === self.cate_index :
+                    el
+            });
+        },
+
+        /**
+         * @method 切换分类
+         * @param {Number} index 需要切换类目的数组下标
+         */
         toggleCate: function(index) {
             this.cate_index = index;
             // 分类操作
@@ -128,25 +133,23 @@ new Vue({
             // 商品排序
             this.sortBy(this.filter_index);
         },
-        // 商品分类
-        setList: function() {
-            var self = this;
-            this.list = this.goods.filter(function(el) {
-                return self.cate_index !== 0 ?
-                    el.type === self.cate_index :
-                    el;
-            });
-        },
-        // 条件比较器
-        compare: function(prop, order) {
+
+        /**
+         * 根据属性值进行升序或降序的比较器
+         * 
+         * @method 属性比较器
+         * @param {String} prop 属性名
+         * @param {String} type 排序方法 (desc: 降序, asc: 升序)
+         */
+        compare: function(prop, type) {
             return function(obj1, obj2) {
-                order = order || 'desc';
+                type = type || 'desc';
 
                 var flag1, flag2;
-                if (order === 'desc') {
+                if (type === 'desc') {
                     flag1 = -1;
                     flag2 = 1;
-                } else if (order === 'asc') {
+                } else if (type === 'asc') {
                     flag1 = 1;
                     flag2 = -1;
                 }
@@ -162,17 +165,26 @@ new Vue({
                 }
             }
         },
-        // 按销量排序
+
+        /**
+         * @method 按销量排序
+         */
         sortSales: function() {
             this.list.sort(this.compare('sales'));
         },
-        // 按价格排序
+
+        /**
+         * @method 按价格排序
+         */
         sortPrice: function() {
             var type = this.price_isAsc ? 'desc' : 'asc';
             this.list.sort(this.compare('price', type));
             this.price_isAsc = !this.price_isAsc;
         },
-        // 根据排序方式调用对应方法
+
+        /**
+         * @method 排序调度器
+         */
         sortBy: function(index) {
             this.filter_index = index;
             if (index == 0) {
@@ -185,28 +197,56 @@ new Vue({
                 this.sortPrice();
             }
         },
-        // 添加到购物车
+
+        /**
+         * @method 添加到购物车
+         * @param {Object} goods 商品
+         */
         addToCart: function(goods) {
-            // 判断商品是否存在
-            var alreadyIndex = this.cart.findIndex(function(item, index) {
+            var alreadyIndex = Cart.cart.findIndex(function(item, index) {
                 return item.id === goods.id;
             });
             // 如果不存在则添加元素
             if (alreadyIndex === -1) {
-                var length = this.cart.length;
+                var cartIndex = Cart.cart.length;
                 // 购物车商品初始化数量为1
-                this.cart.push(goods);
-                this.$set(this.cart[length], 'quantity', 1);
-                this.$set(this.cart[length], 'subtotal', goods.price.toFixed(1));
-                console.log(this.len);
+                Cart.cart.push(goods);
+                Cart.$set(Cart.cart[cartIndex], 'quantity', 1);
+                Cart.$set(Cart.cart[cartIndex], 'subtotal', goods.price.toFixed(1));
                 return;
             }
-            var alreadyGoods = this.cart[alreadyIndex];
-            // 如果存在，数量加1
-            this.$set(alreadyGoods, 'quantity', ++alreadyGoods.quantity);
-            this.$set(alreadyGoods, 'subtotal', (alreadyGoods.price * alreadyGoods.quantity).toFixed(1));
-        },
-        // 修改单品数量 add/sub
+            var alreadyGoods = Cart.cart[alreadyIndex];
+            var num = alreadyGoods.quantity,
+                stock = alreadyGoods.stock;
+            // 如果商品已存在并且库存足够，数量加1
+            if (num < stock) {
+                Cart.$set(alreadyGoods, 'quantity', ++alreadyGoods.quantity);
+                Cart.$set(alreadyGoods, 'subtotal', (alreadyGoods.price * alreadyGoods.quantity).toFixed(1));
+            }
+        }
+    }
+});
+
+var Cart = new Vue({
+    el: '#page-cart',
+    data: {
+        cart: [{
+            id: 1001,
+            name: 'Beats EP头戴式耳机',
+            price: 558,
+            type: 4,
+            quantity: 1,
+            subtotal: 558,
+            stock: 128,
+            sales: 1872,
+            img: 'http://img11.360buyimg.com/n1/s528x528_jfs/t3109/194/2435573156/46587/e0e867ac/57e10978N87220944.jpg!q70.jpg'
+        }]
+    },
+    methods: {
+
+        /**
+         * @method 增减单品数量
+         */
         changeQty: function(isAdd, index) {
             var num = this.cart[index].quantity,
                 stock = this.cart[index].stock;
@@ -230,4 +270,4 @@ new Vue({
             return num;
         }
     }
-})
+});
